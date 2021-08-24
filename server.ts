1fbc4e2fc9431325622b9089d6820531256bb328
Denis <tffoem@gmail.com>
2021-08-24 15:52:23 +0300

    WIP: init

--- server.ts
+++ server.ts
@@ -2,15 +2,9 @@ const fastify = require('fastify')({ logger: true })
 
 fastify.register(require('fastify-cors'), {})
 
-fastify.get('/', async (request, response) => {
-  return { hello: 'world' }
-})
+fastify.get('/', async (request, response) => ({ hello: 'world' }))
 
-fastify.get('/fastify', async (request, response) => {
-  return { hello: 'beautiful world' }
-})
-
-fastify.listen(3000, function (err, address) {
+fastify.listen(3000, (err, address) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
