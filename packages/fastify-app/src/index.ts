2d3470739c23c58778870113241f32a8256c3602
Denis <tffoem@gmail.com>
2021-08-26 15:15:58 +0300

    refactor: project incapsulation

--- packages/fastify-app/src/index.ts
+++ packages/fastify-app/src/index.ts
@@ -1,12 +1,13 @@
-import fastify from "fastify";
+import fastify from "fastify"
+import cors    from "fastify-cors"
 
 const server = fastify({logger: true})
 
-server.register(require('fastify-cors'), {})
+server.register(cors)
 
 server.get('/', async (request, response) => ({ hello: 'world' }))
 
-server.listen(3000, process.env.MODE === 'DEV' ? "0.0.0.0" : "127.0.0.1", (err, address) => {
+server.listen(process.env.PORT || 3000, process.env.MODE === 'DEV' ? "0.0.0.0" : "127.0.0.1", (err, address) => {
   if (err) {
     server.log.error(err)
     process.exit(1)
