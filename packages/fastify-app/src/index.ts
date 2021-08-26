a179eb62c391413b5d192653a20bc25fe33a94c8
Denis <tffoem@gmail.com>
2021-08-26 14:23:57 +0300

    feat: put fastify server into docker container

--- packages/fastify-app/src/index.ts
+++ packages/fastify-app/src/index.ts
@@ -6,7 +6,7 @@ server.register(require('fastify-cors'), {})
 
 server.get('/', async (request, response) => ({ hello: 'world' }))
 
-server.listen(3000, (err, address) => {
+server.listen(3000, process.env.MODE === 'DEV' ? "0.0.0.0" : "127.0.0.1", (err, address) => {
   if (err) {
     server.log.error(err)
     process.exit(1)
